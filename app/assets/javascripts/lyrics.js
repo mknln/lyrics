60a07c95deec879f204aa6f4a0e8ec7e3425293e
Mike Nolan <mpnolan@gmail.com>
2013-05-19 22:38:16 -0500

    Add autocomplete for song titles as well. A bit rough, and still needs to handle when the user enters a gibberish artist name.

--- app/assets/javascripts/lyrics.js
+++ app/assets/javascripts/lyrics.js
@@ -1,5 +1,29 @@
 
+$.ajaxSettings.traditional = true;
+
 $(document).ready(function() {
+
+  var reset_title_autocomplete = function() {
+    $.ajax({
+      url: "http://developer.echonest.com/api/v4/artist/songs",
+      dataType: "jsonp",
+      data: {
+        api_key: "5OIRDLQ1VB9GCZUTX",
+        format: "jsonp",
+        name: $("#lyric_artist").val(),
+        results: 100
+      },
+      success: function( data ) {
+        if (data.response.songs) {
+          var songs = $.map(data.response.songs, function( item ) {
+            return item.title;
+          });
+
+          $("#lyric_title").autocomplete("option", { source: songs })
+        }
+      }
+    });
+  };
 
   $("#lyric_artist").autocomplete({
     source: function(request, response) {
@@ -14,15 +38,26 @@ $(document).ready(function() {
           results: 10
         },
         success: function( data ) {
-          //console.log(data);
           response($.map(data.response.artists, function( item ) {
             return {
               label: item.name,
               value: item.name
             }
           }));
-        }
+        },
       });
+    },
+    select: function(event, ui) {
+      reset_title_autocomplete();
     }
   });
+
+  $("#lyric_title").autocomplete({
+    source: []
+  });
+
+  if ($("#lyric_artist").val()) {
+    reset_title_autocomplete();
+  }
+
 });
