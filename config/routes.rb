68e118feaaff50bcbb26bb677bce07574531e549
Mike Nolan <mpnolan@gmail.com>
2013-05-25 16:23:53 -0500

    Revert "Create the Playlist model and link it to Lyrics"
    This reverts commit 7d5e88fbd0e213c2c952554222d63e6559452bb5.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Lyrics::Application.routes.draw do
 
-  resources :playlists
-
-
   resources :lyrics
 
   root :to => 'lyrics#index'
--- db/migrate/20130525013839_create_lyric_playlist_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateLyricPlaylistJoinTable < ActiveRecord::Migration
-  def change
-    create_table :lyrics_playlists, :id => false do |t|
-      t.integer :lyric_id
-      t.integer :playlist_id
-    end
-  end
-end
