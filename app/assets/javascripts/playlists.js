4c724dc065403626157e4f6589f86aa953970342
Mike Nolan <mpnolan@gmail.com>
2013-05-26 01:23:52 -0500

    autocomplete now works with dynamically added elements

--- app/assets/javascripts/playlists.js
+++ app/assets/javascripts/playlists.js
@@ -1,11 +1,19 @@
 
 $(document).ready(function() {
-  $(".lyric_id_search").autocomplete({
+  var options = {
     source: "/lyrics/search.json",
     select: function(event, ui) {
       $(this).prevAll(".lyric_id").val(ui.item.id);
     }
+  };
+
+  $(".lyric_id_search").autocomplete(options);
+
+  $(document).on("focus", ".lyric_id_search"/*:not(.ui-autocomplete-input)"*/, function (event) {
+    console.log("yeah, bro");
+    $(this).autocomplete(options);
   });
+  
 });
 
 function remove_fields(link) {
@@ -15,7 +23,8 @@ function remove_fields(link) {
 function add_fields(link, association, content) {
   var html = '<div class="field">';
   html += '<label for="playlist_lyric_ids_">Lyric ID: </label>';
-  html += '<input id="playlist_lyric_ids_" name="playlist[lyric_ids][]" size="30" type="text" value="">';
+  html += '<input class="lyric_id" id="playlist_lyric_ids_" name="playlist[lyric_ids][]" type="hidden">';
+  html += '<input class="lyric_id_search" id="playlist_lyric_ids_" name="playlist[lyric_ids][]" size="30" type="text" value="">';
   html += ' <a href="#" onclick="remove_fields(this); return false;">remove</a>';
   html += '</div>';
   
