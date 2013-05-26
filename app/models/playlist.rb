834565ab01c5478ec46905ac2fdccac9211df35b
Mike Nolan <mpnolan@gmail.com>
2013-05-25 22:46:27 -0500

    filter out invalid or blank lyric ids before saving

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,11 @@ class Playlist < ActiveRecord::Base
   attr_accessible :lyric_ids, :name
   serialize :lyric_ids, Array
 
+  before_save :filter_lyric_ids
+
+  def filter_lyric_ids
+    lyric_ids.reject! { |id| id.blank? }
+    lyric_ids.reject! { |id| !Lyric.exists?(id) }
+  end
+
 end
