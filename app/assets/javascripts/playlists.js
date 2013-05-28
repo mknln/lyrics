3051ccbe0781936c007834d7b7907ca0d1943faf
Mike Nolan <mpnolan@gmail.com>
2013-05-28 01:48:53 -0500

    Add a countdown timer to the playlist player when skipping songs

--- app/assets/javascripts/playlists.js
+++ app/assets/javascripts/playlists.js
@@ -1,15 +1,40 @@
 
+function countdown_to_page_skip() {
+  if ($("#next-lyric-id").length > 0) {
+
+    var stop_timer = false;
+    $(".stop-timer").click(function(e) {
+      e.preventDefault();
+      stop_timer = true;
+    });
+
+    $("#countdown-timer").show();
+    var counter = function(n) {
+      if (stop_timer) {
+        return;
+      }
+
+      if (n == 0) {
+        window.location = window.location.pathname + "?lyric_id=" + $("#next-lyric-id").val();
+      } else {
+        $("#countdown-timer .time").html(n);
+        setTimeout(function() { counter(n - 1); }, 1000);
+      }
+    }
+    counter(5);
+  }
+}
 
 $(document).ready(function() {
 
   window.onYouTubePlayerAPIReady = function() {
-    if ($(".video-player #youtube-id")) {
+    if ($(".video-player #youtube-id").length > 0) {
       var id = $(".video-player #youtube-id").html();
       new YT.Player('player_' + id, {
         events: {
           'onStateChange': function(event) {
             if (event.data == YT.PlayerState.ENDED) {
-              if ($("#next-lyric-id") && $("#next-lyric-id").val()) {
+              if ($("#next-lyric-id").length > 0) {
                 window.location = window.location.pathname + "?lyric_id=" + $("#next-lyric-id").val();
               }
               // forward them to the next video
@@ -20,6 +45,8 @@ $(document).ready(function() {
           }
         }
       });
+    } else {
+      countdown_to_page_skip();
     }
   };
 
