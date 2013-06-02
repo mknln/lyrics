f0751ee87453c4264d33d64c8bf669b0a49986dc
Mike Nolan <mpnolan@gmail.com>
2013-06-02 02:21:56 -0500

    Add thumbnails and list songs on playlists page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def youtube_embed(id)
     "<iframe id='player_#{id}' width='420' height='315' src='http://www.youtube.com/embed/#{id}' frameborder='0' allowfullscreen></iframe>"
   end
+
+  def youtube_thumb(id)
+    "<img src='#{youtube_thumb_url(id)}' />"
+  end
+
+  def youtube_thumb_url(id)
+    "http://img.youtube.com/vi/#{id}/default.jpg"
+  end
 end
